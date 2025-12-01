c51f7fc32818a5e65524da2de54469ef492e0c80
VS-GitHub-001 <105536514+VS-GitHub-001@users.noreply.github.com>
2025-12-01 22:17:51 +0100

    update

---
+++ ChoosenCareHome/Pages/Services/SupportedLiving.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ChoosenCareHome.Pages.Services
+{
+    public class SupportedLivingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
